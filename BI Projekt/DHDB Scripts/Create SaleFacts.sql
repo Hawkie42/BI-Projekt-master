a5e04137eaff245cfb319e118204cf9a8760bcf6
tim schjørring <blackti1@live.dk>
2018-05-25 12:29:50 +0200

    FUCKING ETL VIRK

--- BI Projekt/DHDB Scripts/Create SaleFacts.sql
+++ BI Projekt/DHDB Scripts/Create SaleFacts.sql
@@ -1,16 +1,13 @@
-BEGIN TRY
-	DROP TABLE Sale_FACTS
-END TRY
 
-BEGIN CATCH
-/*BEGONE THOT*/
-END CATCH
 
 CREATE TABLE Sale_FACTS(
 	Sale_ID int primary key IDENTITY (1,1),
-	FK_Product_ID int,
-	FK_Member_ID int,
-	FK_Date_ID int,
-	FK_Timestamp_ID int,
-	Sale_Price float,
+	CONSTRAINT FK_Product_ID FOREIGN KEY(Product_ID)
+	REFERENCES DIM_Product(Product_ID),
+	CONSTRAINT FK_Member_ID FOREIGN KEY(Member_ID)
+	REFERENCES DIM_Member(Member_ID),
+	CONSTRAINT FK_Date_ID FOREIGN KEY(DateKey)
+	REFERENCES DimDate(DateKey),
+	CONSTRAINT FK_Time_ID FOREIGN KEY(TimeKey)
+	REFERENCES DimTime(TimeKey),
 )
